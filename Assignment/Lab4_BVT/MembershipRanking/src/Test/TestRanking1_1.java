be4091ad21baf3a554ac561d7b1bb5e015f578dd
Prew <jularat.k@kkumail.com>
2025-07-19 13:12:34 +0700

    แก้ไข csv

--- Assignment/Lab4_BVT/MembershipRanking/src/Test/TestRanking1_1.java
+++ Assignment/Lab4_BVT/MembershipRanking/src/Test/TestRanking1_1.java
@@ -11,19 +11,19 @@ public class TestRanking1_1 {
     @ParameterizedTest
     @CsvSource({
         // purchase, freq, point, expectedRank
-        "10000, 4, 500, Silver",
-        "10001, 4, 500, Silver",
-        "45000, 4, 500, Silver",
-        "99999, 4, 500, Gold",
-        "100000, 4, 500, Gold",
-        "45000, 1, 500, Silver",
-        "45000, 2, 500, Silver",
-        "45000, 6, 500, Silver",
-        "45000, 7, 500, Silver",
-        "45000, 4, 100, Silver",
-        "45000, 4, 250, Silver",
-        "45000, 4, 750, Silver",
-        "45000, 4, 1000, Silver",
+    	"0, 15, 500, Standard",
+    	"1, 15, 500, Standard",
+    	"50000, 15, 500, Gold",
+    	"99999, 15, 500, Gold",
+    	"100000, 15, 500, Gold",
+    	"50000, 1, 500, Silver",
+    	"50000, 2, 500, Silver",
+    	"50000, 30, 500, Gold",
+    	"50000, 31, 500, Gold",
+    	"50000, 15, 0, Standard",
+    	"50000, 15, 1, Standard",
+    	"50000, 15, 999, Gold",
+    	"50000, 15, 1000, Gold"
 
     })
     void testRankingFromCSV(int purchase, int freq, int point, String expectedRank) {
--- Assignment/Lab4_BVT/MembershipRanking/src/Test/TestRanking1_2.java
+++ Assignment/Lab4_BVT/MembershipRanking/src/Test/TestRanking1_2.java
@@ -15,27 +15,26 @@ class TestRanking1_2 {
     @ParameterizedTest
     @CsvSource({
         // purchaseTotal, frequency, points, expectedRank
-        "0,4,500,Standard",          // min–1 (invalid)
-        "10000,4,500,Silver",       // min
-        "10001,4,500,Silver",       // min+1
-        "45000,4,500,Silver",       // nominal
-        "99999,4,500,Gold",       // max-1
-        "100000,4,500,Gold",      // max
-        "100099,4,500,Gold",      // max+1 (invalid but system allows)
+    	"0, 15, 500, Standard",
+    	"0, 15, 500, Standard",
+    	"1, 15, 500, Standard",
+    	"50000, 15, 500, Gold",
+    	"99999, 15, 500, Gold",
+    	"100000, 15, 500, Gold",
+    	"100001, 15, 500, Gold",
+    	"50000, 0, 500, Standard",
+    	"50000, 1, 500, Silver",
+    	"50000, 2, 500, Silver",
+    	"50000, 30, 500, Gold",
+    	"50000, 31, 500, Gold",
+    	"50000, 32, 500, Gold",
+    	"50000, 15, 0, Standard",
+    	"50000, 15, 0, Standard",
+    	"50000, 15, 1, Standard",
+    	"50000, 15, 999, Gold",
+    	"50000, 15, 1000, Gold",
+    	"50000, 15, 1001, Gold"
 
-        "45000,0,500,Standard",     // min–1 (invalid)
-        "45000,1,500,Silver",     // min
-        "45000,2,500,Silver",       // min+1
-        "45000,6,500,Silver",       // max-1
-        "45000,7,500,Silver",       // max
-        "45000,8,500,Silver",       // max+1
-
-        "45000,4,50,Standard",      // min–1 (invalid)
-        "45000,4,100,Silver",       // min
-        "45000,4,250,Silver",       // min+1
-        "45000,4,750,Silver",       // max-1
-        "45000,4,1000,Silver",      // max
-        "45000,4,1001,Silver"       // max+1
     })
     void testRobustBoundary(int purchase, int freq, int points, String expected) {
         assertEquals(expected, ranker.CalculateMembershipRank(purchase, freq, points));
