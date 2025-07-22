f555a9fc8714228c646136ab6a3b49193eebc4a5
Prew_cp9422 <jularat.k@kkumail.com>
2025-07-22 14:56:49 +0700

    แก้ไขนิดหน่อย
    22/7/2025

---
+++ Assignment/Lab5_EC/FactoryProblem/src/Test/Test_class_Income.java
@@ -0,0 +1,14 @@
+	package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Test_class_Income {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
